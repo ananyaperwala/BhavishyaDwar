33133410b4de23bdef119210b08133712b85d5bb
Ananya Perwala <21bd1a0504csea@gmail.com>
2023-02-08 11:15:18 +0530

    Variable change in Fetch.js

--- src/Fetch.js
+++ src/Fetch.js
@@ -5,14 +5,14 @@ mongoose.set('strictQuery', false);
 const mongoURL = "mongodb+srv://user1:kmit1@cluster0.0csl8ss.mongodb.net/BhavishyaDwar?retryWrites=true&w=majority";
 mongoose.connect(mongoURL).then(console.log('Database Connected')).catch('Database is not connected');
 const Schema = mongoose.Schema;
-const itemSchema = new Schema({
+const itemsSchema = new Schema({
     name:String,
     about: String
     
 }, {
     collection:"Users"
 })
-const User = mongoose.model(" ",itemSchema);
+const User = mongoose.model(" ",itemsSchema);
 User.find({},(err, items) =>{
     if(err){
             console.log(err);
